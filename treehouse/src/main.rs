ae9d0f67d6d5f38dc7cfd2af1f88064d6e2d3743
Matt Lucas <matt.lucas@nagra.com>
2020-12-12 17:36:59 +0000

    Finished treehouse projecct

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,44 +1,81 @@
 #![warn(clippy::all, clippy::pedantic)]
 use std::io::stdin;
 
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation
+}
+
+#[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String,
+    action: VisitorAction,
+    age: i8
 }
 
 impl Visitor {
 
-    fn new(name:&str, greeting:&str) -> Self {
+    fn new(name:&str, action:VisitorAction, age:i8) -> Self {
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string()
+            action,
+            age
         }
     }
 
     fn greet(&self) {
-        println!("{}", self.greeting);
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the tree house, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the tree house, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not serve alcohol to {}", self.name);
+                }
+            },
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name)
+        }
     }
 
 }
 
 fn main() {
-    println!("Hello, what's your name?");
-    let your_name = what_is_your_name();
-    let mut visitor:Option<&Visitor> = None;
-    let visitors = [
-        Visitor::new("bert", "Hello Bert, enjoy your treehouse"),
-        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
-        Visitor::new("fred", "Wow, who invited Fred?"),
+
+    let mut visitors = vec![
+        Visitor::new("bert", VisitorAction::Accept, 45),
+        Visitor::new("steve", VisitorAction::AcceptWithNote {note: String::from("Lactose-free milk is in the fridge")}, 15),
+        Visitor::new("fred", VisitorAction::Refuse, 30),
     ];
-    for v in visitors.iter() {
-        if v.name == your_name {
-            visitor = Option::from(v);
+
+    loop {
+
+        println!("Hello, what's your name?");
+        let name = what_is_your_name();
+
+        let visitor = visitors
+            .iter()
+            .find(|v| v.name == name);
+
+        match visitor {
+            None => {
+                if name.is_empty() {
+                    break;
+                }
+                println!("{} is not on the visitor list.", name);
+                visitors.push(Visitor::new(&name, VisitorAction::Probation, 0))
+            }
+            Some(v) => v.greet(),
         }
+
     }
-    match visitor {
-        None => println!("Sorry, you aren't on the list"),
-        Some(v) => v.greet(),
-    }
+
+    println!("The final list of visitors:");
+    println!("{:#?}", visitors);
+
 }
 
 fn what_is_your_name() -> String {
