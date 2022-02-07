12ecb36cb3046adee5d1ac498bbeafc84959a2f9
Matt Lucas <matt.lucas@nagra.com>
2022-02-07 17:16:09 +0000

    init flappy

---
+++ flappy/src/main.rs
@@ -0,0 +1,21 @@
+use bracket_lib::prelude::*;
+
+struct State {
+
+}
+
+impl GameState for State {
+
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+    main_loop(context, State{})
+}
